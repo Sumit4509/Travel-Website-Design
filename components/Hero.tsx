7f73f25bc8e50e96855904f3429aaf43c02988d7
Sumit <104025799+Sumit4509@users.noreply.github.com>
2024-07-31 01:45:43 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -61,7 +61,7 @@ const Hero = () => {
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
+            <p className="bold-20 text-white">Mainpat</p>
           </div>
 
           <div className="flexBetween">
