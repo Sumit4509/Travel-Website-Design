d4518ff81357b44eec581888d3e91e8254f5d250
Sumit <104025799+Sumit4509@users.noreply.github.com>
2024-07-31 19:28:00 +0530

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -36,14 +36,13 @@ const Guide = () => {
             <div className='flex w-full flex-col'>
               <div className="flexBetween w-full">
                 <p className="regular-16 text-gray-20">Destination</p>
-                <p className="bold-16 text-green-50">48 min</p>
+                <p className="bold-16 text-green-50">1 hr</p>
               </div>
-              <p className="bold-20 mt-2">Aguas Calientes</p>
+              <p className="bold-20 mt-2">Mainpat</p>
             </div>
 
             <div className='flex w-full flex-col'>
-              <p className="regular-16 text-gray-20">Start track</p>
-              <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</h4>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">Ambikapur</h4>
             </div>
           </div>
         </div>
