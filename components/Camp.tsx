4cb0754257c70328f762c746375378ebb07544d8
Sumit <104025799+Sumit4509@users.noreply.github.com>
2024-07-31 15:33:02 +0530

    Update Camp.tsx

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -52,7 +52,7 @@ const Camp = () => {
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
       <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite 
-          backgroundImage="bg-bg-img-11"
+          backgroundImage="bg-bg-img-1"
           title="Putuk Truno Camp"
           subtitle="Prigen, Pasuruan"
           peopleJoined="50+ Joined"
