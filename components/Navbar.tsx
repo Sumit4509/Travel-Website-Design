79f228bfd8fd1070697807a5063aa391718e0cc1
Sumit <104025799+Sumit4509@users.noreply.github.com>
2024-08-01 00:44:39 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+        <Image src="/hilink-logo.svg" alt="logo" width={100} height={100} />
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
