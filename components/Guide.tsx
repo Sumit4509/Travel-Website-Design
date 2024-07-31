3ff14d86162f210ae9a281af1bf9804014b4ff67
Sumit <104025799+Sumit4509@users.noreply.github.com>
2024-08-01 00:51:42 +0530

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -11,7 +11,7 @@ const Guide = () => {
         </p>
         <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
           <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Guide You to Easy Path</h2>
-          <p className="regular-16 text-gray-30 xl:max-w-[520px]">Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">Only with the EXPLORE CG application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
         </div>
       </div>
 
