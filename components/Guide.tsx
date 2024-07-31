b05a70bfcd6dc312dbe43abdf63f3a61d5672580
Sumit <104025799+Sumit4509@users.noreply.github.com>
2024-07-31 19:31:04 +0530

    Update Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -42,6 +42,7 @@ const Guide = () => {
             </div>
 
             <div className='flex w-full flex-col'>
+                <p className="regular-16 text-gray-20">Start track</p>
               <h4 className="bold-20 mt-2 whitespace-nowrap">Ambikapur</h4>
             </div>
           </div>
